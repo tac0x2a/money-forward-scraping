2006acd5f00420555e959e1b33a334e64ca97b7c
TAC <tac@tac42.net>
2015-01-04 23:26:37 +0900

    Implement Creatie cache flow
    Not implemented large/middle category.

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 require "./money_forward_scraper"
 
-p MFSUtil.total_assets
+# p MFSUtil.total_assets
+p MFSUtil.create_cacheflow(amount: 1000, content: "Sample Amount")
--- money_forward_scraper.rb
+++ money_forward_scraper.rb
@@ -9,6 +9,7 @@ class MoneyForwardScraper
   @@SIGN_IN_URL = "https://moneyforward.com/users/sign_in"
   @@SIGN_OUT_URL = "https://moneyforward.com/users/sign_out"
   @@DEFAULT_URL = "https://moneyforward.com/"
+  @@CACHE_FLOW_URL = "https://moneyforward.com/cf"
 
   def initialize
     @agent = Mechanize.new
@@ -42,6 +43,16 @@ class MoneyForwardScraper
     end
     value
   end
+
+  def create_cacheflow(updated_at, amount, content)
+    page = @agent.get(@@CACHE_FLOW_URL)
+    page.form_with(:action =>  '/cf/create') do |form|
+      form.field_with(:name => 'user_asset_act[updated_at]').value = updated_at
+      form.field_with(:name => 'user_asset_act[amount]').value = amount.to_s
+      form.field_with(:name => 'user_asset_act[content]').value = content
+    end.submit
+    {updated_at: updated_at, amount: amount, content: content}
+  end
 end
 
 class MFSUtil
@@ -49,6 +60,14 @@ class MFSUtil
     mfs = MoneyForwardScraper.new
     mfs.sign_in
     value = mfs.total_assets
+    mfs.sign_out
+    value
+  end
+
+  def MFSUtil.create_cacheflow(updated_at: Time.now.strftime("%Y/%m/%d"), amount: '0', content: '')
+    mfs = MoneyForwardScraper.new
+    mfs.sign_in
+    value = mfs.create_cacheflow(updated_at, amount, content)
     mfs.sign_out
     value
   end
